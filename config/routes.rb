ebb9c1653fa7d48a1c398b3e554aa7408b37aafe
Jack Chung <jack@jackchung.com>
2015-10-28 20:02:29 +0000

    lecture 56

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,20 @@ Rails.application.routes.draw do
   
   get '/home', to: 'pages#home'
   
-  resources :recipes
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
+  
+  resources :chefs, except: [:new]
+  
+  get '/register', to: 'chefs#new'
+  
+  get '/login', to: 'logins#new'
+  post '/login', to: 'logins#create'
+  get '/logout', to: 'logins#destroy'
+  
   #get '/recipes', to: 'recipes#index'
   #get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
   #post '/recipes', to: 'recipes#create'
