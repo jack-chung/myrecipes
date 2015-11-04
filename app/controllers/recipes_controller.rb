19feef70d94c0a07be42646266ee6794f029e555
Jack Chung <jack@jackchung.com>
2015-11-04 18:31:56 +0000

    chapter 72

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,12 +3,14 @@ class RecipesController < ApplicationController
   before_action :require_user, except: [:show, :index, :like]
   before_action :require_user_like, only: [:like]
   before_action :require_same_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
   
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 4)
   end
   
   def show
+
   end
   
   def new
@@ -52,6 +54,12 @@ class RecipesController < ApplicationController
     
   end
   
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe Deleted"
+    redirect_to recipes_path
+  end
+  
   private
     def recipe_params
       params.require(:recipe).permit(:name, :summary, :description, :picture, style_ids: [], ingredient_ids: [])
@@ -62,16 +70,20 @@ class RecipesController < ApplicationController
     end
     
     def require_same_user
-      if current_user != @recipe.chef
+      if current_user != @recipe.chef and !current_user.admin?
         flash[:danger] = "You can only edit your own recipes"
         redirect_to recipe_path
       end
     end
     
     def require_user_like
-    if !logged_in?
-      flash[:danger] = "You must be logged in to perform that action"
-      redirect_to :back
+      if !logged_in?
+        flash[:danger] = "You must be logged in to perform that action"
+        redirect_to :back
+      end
     end
-  end
+    
+    def admin_user
+      redirect_to recipes_path unless current_user.admin?
+    end
 end
